6a788faddae30b6bf03a8d30860668e58bfcf4df
Marcus Ng <marcuslistanng@gmail.com>
2018-05-14 00:25:52 -0400

    3D Touch

--- PixelCity/Utilities/Constants.swift
+++ PixelCity/Utilities/Constants.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Marcus Ng. All rights reserved.
 //
 
-let apiKey = "903fb47b981b71bcaa600362da5fe8a2"
+let apiKey = "<FLICKR_APIKEY>"
 
 func flickrURL(forApiKey key: String, withAnnotation annotation: DroppablePin, andNumberOfPhotos number: Int) -> String {
     return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(apiKey)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
